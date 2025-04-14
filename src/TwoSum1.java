e0ddcaed348b618ac9d157aab15b35dc5f0db5d2
broxy-a <abdrahmanovamir2001@gmail.com>
2025-04-14 16:46:15 +0500

    Обновил TwoSum1 и добавил PalindromeNumber

--- src/TwoSum1.java
+++ src/TwoSum1.java
@@ -15,7 +15,7 @@ public class TwoSum1 {
 
     public static void main(String[] args) throws Exception {
         int[] nums = {2, 7, 11, 15};
-        int target = 10;
+        int target = 9;
         int[] result = new TwoSum1().twoSum(nums, target);
         System.out.println(Arrays.toString(result));
     }
